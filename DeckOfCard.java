fa9a8073c7ea2d495221f176a8ff2e7b108ef906
Ganesh <ganeshgavhad9@gmail.com>
2021-07-26 09:18:46 +0530

    Added : UC1 of unique card and no of card

--- DeckOfCard.java
+++ DeckOfCard.java
@@ -1,9 +1,18 @@
+/**
+ * Purpose : Second Week Workshop
+ * @author Ganesh Gavhad
+ *@version 1.0
+ */
 package com.bridgelabz;
 
+import java.util.ArrayList;
+
 public class DeckOfCard {
+	String[] suits = new String[] { "CLUB", "DIAMOND", "HEART", "SPADE" };
+	String[] ranks = new String[] { "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace" };
+	ArrayList<Card> deck = new ArrayList<>(52);
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to deck of Card");
 	}
-
 }
